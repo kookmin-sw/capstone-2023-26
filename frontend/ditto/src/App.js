53be9676e681600a6a1a10679de3b55ad128d6b2
soyekwon <soye0710@naver.com>
2023-04-06 15:36:40 +0900

    feat: Create main UI

--- frontend/ditto/src/App.js
+++ frontend/ditto/src/App.js
@@ -1,22 +1,22 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-// import { useNavigate } from "react-router-dom";
-// import { useEffect } from 'react';
 import { useState } from 'react';
-import Buttons from "./components/Buttons";
-import Map from "./components/map";
+import Home from "./components/Home";
+import Area from "./components/Area";
+import Place from "./components/Place";
+import Map from "./components/Map";
 
 const App = () =>{
-  // const navigate = useNavigate();
   const [x, setX] = useState(37.61066839994)
   const [y, setY] = useState(126.9973271351)
+  const [id, setId] = useState(0)
 
-
-  
   return (
     <>
      <Router>
      <Routes>
-        <Route path="/" element={<Buttons changeX={setX} changeY={setY} />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/area" element={<Area id={id} changeId={setId}/>} />
+        <Route path="/place" element={<Place x={x} y={y} changeX={setX} changeY={setY} id={id}/>} />
         <Route path="/map" element={<Map x={x} y={y}/>} />
       </Routes>
 
--- frontend/ditto/src/components/map.js
+++ frontend/ditto/src/components/map.js
@@ -1,45 +1,83 @@
 /*global kakao*/ 
 import React, { useEffect } from 'react'
+import FlexBox from "./Common/FlexBox";
 
 const Map =(props)=> {
   useEffect(()=>{
     var container = document.getElementById('map');
     var options = {
       center: new kakao.maps.LatLng(props.x , props.y),
-      level: 2
+      level: 3
     };
     var map = new kakao.maps.Map(container, options);
 
-    var markerPosition  = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488); 
-    var marker = new kakao.maps.Marker({
-      position: markerPosition
-    });
-    marker.setMap(map);
-      
-    var polygonPath = [
-      new kakao.maps.LatLng(33.45133510810506, 126.57159381623066),
-      new kakao.maps.LatLng(33.44955812811862, 126.5713551811832),
-      new kakao.maps.LatLng(33.449986291544086, 126.57263296172184),
-      new kakao.maps.LatLng(33.450682513554554, 126.57321034054742),
-      new kakao.maps.LatLng(33.451346760004206, 126.57235740081413) 
-    ];
-    var polygon = new kakao.maps.Polygon({
-      path:polygonPath, // 그려질 다각형의 좌표 배열입니다
-      strokeWeight: 3, // 선의 두께입니다
-      strokeColor: '#39DE2A', // 선의 색깔입니다
-      strokeOpacity: 0.8, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
-      strokeStyle: 'longdash', // 선의 스타일입니다
-      fillColor: '#A2FF99', // 채우기 색깔입니다
-      fillOpacity: 0.7 // 채우기 불투명도 입니다
-    });
-    polygon.setMap(map);
+    var circle = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.6109026256, 126.9961832276),  // 원의 중심좌표 입니다 
+      radius: 26, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ff001691', // 채우기 색깔입니다
+      fillOpacity: 0.8  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle.setMap(map);
+
+    var circle2 = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.61130804, 126.9954130528),  // 원의 중심좌표 입니다 
+      radius: 18, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ffac0091', // 채우기 색깔입니다
+      fillOpacity: 0.8  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle2.setMap(map);
+
+    var circle3 = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.6105084509, 126.99644090),  // 원의 중심좌표 입니다 
+      radius: 17, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ffac0091', // 채우기 색깔입니다
+      fillOpacity: 0.8  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle3.setMap(map);
+
+    var circle4 = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.6106526312, 126.9972676758),  // 원의 중심좌표 입니다 
+      radius: 14, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ff001691', // 채우기 색깔입니다
+      fillOpacity: 0.8  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle4.setMap(map);
+
+    var circle5 = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.6113193719, 126.9976753801),  // 원의 중심좌표 입니다 
+      radius: 15, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ffac0091', // 채우기 색깔입니다
+      fillOpacity: 0.6  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle5.setMap(map);
+
+    var circle6 = new kakao.maps.Circle({
+      center : new kakao.maps.LatLng(37.61193652435, 126.996633378),  // 원의 중심좌표 입니다 
+      radius: 18, // 미터 단위의 원의 반지름입니다 
+      strokeOpacity: 0, // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+      fillColor: '#ff001691', // 채우기 색깔입니다
+      fillOpacity: 0.8  // 채우기 불투명도 입니다   
+    }); 
+  
+    circle6.setMap(map);
+  
   
   },)
 
     return (
-        <>
+        <FlexBox width="auto">
           <div id="map" style={{width:"100%", height:"100vh"}}></div>
-        </>
+        </FlexBox>
     )
 }
 
