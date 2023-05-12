afc7b1d9f698c1ddb5a78603880c5bd59af15206
Yunseok-Kang <ysck73@gmail.com>
2023-05-12 18:40:27 +0900

    드론 소프트웨어 제어 스켈레톤 코드 추가

---
+++ drone/RPi/drone.py
@@ -0,0 +1,15 @@
+from dronekit import connect, VehicleMode, LocationGlobalRelative,APIException
+import time
+import socket
+import math
+import argparse
+
+class Drone:
+    def __init__(self):
+        pass
+
+    def get_attitude(self):
+        pass
+
+    def get_position(self):
+        pass
