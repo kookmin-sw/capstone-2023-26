49104e3d53687244b7b00aca60e6b069f5dcece5
soyekwon <soye0710@naver.com>
2023-05-13 02:41:15 +0900

    style: 관리자 페이지 css 수정

--- ditto/static/data/scripts.js
+++ ditto/static/data/scripts.js
@@ -80,73 +80,109 @@ window.Apex = {
   }
   
   new ApexCharts(document.querySelector("#spark3"), spark3).render();
-  
-  var optionsLine = {
+
+  var optionsA = {
+	series: [{
+	name: '사람 수',
+	data: [3, 33, 21, 42, 19, 32, 40],
+	color: '#f02fc2'
+  }, ],
+  title: {
+		  text: '시간별 사람수',
+		  align: 'left',
+		  offsetY: 25,
+		  offsetX: 20,
+		},
 	chart: {
-	  height: 328,
-	  type: 'line',
-	  zoom: {
-		enabled: false
-	  },
-	  dropShadow: {
-		enabled: true,
-		top: 3,
-		left: 2,
-		blur: 4,
-		opacity: 1,
-	  }
+	height: 330,
+	type: 'area'
+  },
+  dataLabels: {
+	enabled: false
+  },
+  stroke: {
+	curve: 'smooth'
+  },
+  xaxis: {
+	type: 'datetime',
+	categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+  },
+  tooltip: {
+	x: {
+	  format: 'dd/MM/yy HH:mm'
 	},
-	stroke: {
-	  curve: 'smooth',
-	  width: 2
-	},
-	//colors: ["#3F51B5", '#2196F3'],
-	series: [
-	  {
-		data: [3, 33, 21, 42, 19, 32],
-		color: '#00e396'
-	  },
-	],
-	title: {
-	  text: '시간별 사람수',
-	  align: 'left',
-	  offsetY: 25,
-	  offsetX: 20,
-	},
-	markers: {
-	  size: 6,
-	  strokeWidth: 0,
-	  hover: {
-		size: 9
-	  }
-	},
-	grid: {
-	  show: true,
-	  padding: {
-		bottom: 0
-	  }
-	},
-	labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
-	xaxis: {
-	  tooltip: {
-		enabled: false
-	  }
-	},
-	legend: {
-	  position: 'top',
-	  horizontalAlign: 'right',
-	  offsetY: -20
-	}
-  }
+  },
+  };
+
+  var chartt = new ApexCharts(document.querySelector("#line-adwords"), optionsA);
+  chartt.render();
   
-  var chartLine = new ApexCharts(document.querySelector('#line-adwords'), optionsLine);
-  chartLine.render();
+//   var optionsLine = {
+// 	chart: {
+// 	  height: 328,
+// 	  type: 'area',
+// 	  zoom: {
+// 		enabled: false
+// 	  },
+// 	  dropShadow: {
+// 		enabled: true,
+// 		top: 3,
+// 		left: 2,
+// 		blur: 4,
+// 		opacity: 1,
+// 	  }
+// 	},
+// 	stroke: {
+// 	  curve: 'smooth',
+// 	  width: 2
+// 	},
+// 	//colors: ["#3F51B5", '#2196F3'],
+// 	series: [
+// 	  {
+// 		data: [3, 33, 21, 42, 19, 32],
+// 		color: '#00e396'
+// 	  },
+// 	],
+// 	title: {
+// 	  text: '시간별 사람수',
+// 	  align: 'left',
+// 	  offsetY: 25,
+// 	  offsetX: 20,
+// 	},
+// 	markers: {
+// 	  size: 6,
+// 	  strokeWidth: 0,
+// 	  hover: {
+// 		size: 9
+// 	  }
+// 	},
+// 	grid: {
+// 	  show: true,
+// 	  padding: {
+// 		bottom: 0
+// 	  }
+// 	},
+// 	labels: ['12:00', '13:00', '14:00', '15:00', '16:00', '17:00'],
+// 	xaxis: {
+// 	  tooltip: {
+// 		enabled: false
+// 	  }
+// 	},
+// 	legend: {
+// 	  position: 'top',
+// 	  horizontalAlign: 'right',
+// 	  offsetY: -20
+// 	}
+//   }
+  
+//   var chartLine = new ApexCharts(document.querySelector('#line-adwords'), optionsLine);
+//   chartLine.render();
   
   var optionsCircle4 = {
 	chart: {
 	  type: 'radialBar',
-	  height: 350,
-	  width: 380,
+	  height: 367,
+	  width: 365,
 	},
 	plotOptions: {
 	  radialBar: {
@@ -170,9 +206,15 @@ window.Apex = {
 	  lineCap: 'round'
 	},
 	title: {
-		text: '일별 사람수',
+		text: '대동제',
 		align: 'left',
 		offsetY: 25,
+		offsetX: 20,
+	  },
+	subtitle: {
+		text: '일별 사람수',
+		align: 'left',
+		offsetY: 48,
 		offsetX: 20,
 	  },
 	series: [71, 63, 77],
@@ -182,16 +224,10 @@ window.Apex = {
 	  floating: true,
 	  position: 'right',
 	  offsetX: 5,
-	  offsetY: 220
+	  offsetY: 252
 	},
   }
   
   var chartCircle4 = new ApexCharts(document.querySelector('#radialBarBottom'), optionsCircle4);
   chartCircle4.render();
   
-  var chartArea = new ApexCharts(
-	document.querySelector("#areachart"),
-	optionsArea
-  );
-  
-  chartArea.render();
