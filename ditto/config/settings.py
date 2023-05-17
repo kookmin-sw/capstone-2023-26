574d33218e10cfae9df5c1b0e81ecc4fb58f05c2
minjunPark <pmj4138@gmail.com>
2023-05-17 21:48:43 +0900

    Update time zone

--- ditto/config/settings.py
+++ ditto/config/settings.py
@@ -114,11 +114,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
