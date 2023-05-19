f642b35449264592ce3de9bbaf6a872606b39566
Yunseok-Kang <ysck73@gmail.com>
2023-05-19 13:23:30 +0900

    드론 시뮬레이터 초기 버전 제작 완료 및 디렉토리 구조 정리

--- drone/Simulator/drone_simulator_ui.py
+++ drone/Simulator/drone_simulator_ui.py
@@ -3,18 +3,22 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
 from PyQt6.QtGui import QImage, QPixmap
 
-import utils_ui
 import threading
 import time
-import ivs_rtmp
+import json
 
-import json # 이거 여기 있는거 맞냐
+import utils.utils_ui
+import networking.ditto_networking
+import networking.ivs_rtmp
+from networking.ditto_networking import Networking
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.rtmp_url = ""
+        self.ditto_backend_url = "http://13.125.5.137:8000"
+        self.rtmp_url = None # 보안 상의 이유로 rtmp url은 지웠습니다.
+        self.networking = Networking()
 
         self.setWindowTitle("Drone Simulator")
         self.resize(800, 600)
@@ -30,7 +34,7 @@ class MainWindow(QMainWindow):
         # 가로로 길쭉한 입력 필드
         self.input_field = QLineEdit()
         self.input_field.setPlaceholderText("ex)")
-        self.input_field.setText("http://13.125.5.137:8000")
+        self.input_field.setText(self.ditto_backend_url)
         main_layout.addWidget(self.input_field)
         # grid_layout.addWidget(input_field, 0, 0, 1, 2)  # 2열로 확장
 
@@ -153,7 +157,7 @@ class MainWindow(QMainWindow):
         self.video_url = video_file
 
         if video_file:
-            thumbnail_data = utils_ui.generate_thumbnail(video_file)
+            thumbnail_data = utils.utils_ui.generate_thumbnail(video_file)
             q_image = QImage(thumbnail_data[0], thumbnail_data[1], thumbnail_data[2], QImage.Format.Format_RGB888)
             thumbnail = QPixmap.fromImage(q_image)
             if thumbnail:
@@ -168,8 +172,8 @@ class MainWindow(QMainWindow):
             self.load_data(json_file)
 
     def connection_check(self):
-        # 불리언 값을 받아와서 상태 원 표시를 변경합니다.
-        is_validated = True  # 여기에서 실제 불리언 값을 받아오는 로직을 작성해야 합니다.
+        # 불리언 값을 받아와서 상태 원 표시를 변경
+        is_validated = self.networking.is_alive(self.ditto_backend_url + "/api/")
 
         if is_validated:
             # self.status_label.setText("Validated")
@@ -184,7 +188,7 @@ class MainWindow(QMainWindow):
         
         for item in self.flight_data:
             label = QLabel()
-            label.setText(f"ID: {item['id']} | Time: {item['time']} | Voltage: {item['voltage']}")
+            label.setText(f"Time: {item['time']} | Voltage: {item['voltage']}")
             self.scroll_layout.addWidget(label)
 
     def send_data_to_server(self):
@@ -192,14 +196,19 @@ class MainWindow(QMainWindow):
         sending_msg_thread.daemon = True
         sending_msg_thread.start()
 
-        print(self.video_url)
-
-        ivs_rtmp.send_video_to_rtmp_server(self.video_url, self.rtmp_url)
+        video_streaming_thread = threading.Thread(target=self.video_streaming_update)
+        video_streaming_thread.daemon = True
+        video_streaming_thread.start()
 
     def sending_msg_update(self):
         for i in range(len(self.flight_data)):
             print(self.flight_data[i])
+            # self.networking.send_msg_to_server_with_params(self.ditto_backend_url + "/api/droneInfo/", params=self.flight_data[i])
+            self.networking.send_msg_to_server_with_params('http://13.125.5.137:8000/api/droneinfo/', self.flight_data[i])
             time.sleep(5) # 임의로 time interval 5로 지정
+
+    def video_streaming_update(self):
+        networking.ivs_rtmp.send_video_to_rtmp_server(self.video_url, self.rtmp_url)
 
 if __name__ == "__main__":
     app = QApplication([])
