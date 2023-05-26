2a4a6aa0b8ef9dc769d2d763fb23f443cb509f3b
minjunPark <pmj4138@gmail.com>
2023-05-26 13:30:53 +0900

    Update re-uploader

--- ditto/loader/views.py
+++ ditto/loader/views.py
@@ -13,9 +13,11 @@ def reUpload():
 
     now = datetime.datetime.now()
 
-    command = f'aws s3 cp s3://ivs-ditto/ivs/v1/392988993234/eknAgsglpDNs/2023/5/12/ {target_dir} --exclude "*" --include "**/160p30/*.ts" --recursive'
+    # 특정일 폴더 하위의 ts파일을 폴더 구조를 유지한 채로 복사
+    command = f'aws s3 cp s3://ivs-ditto/ivs/v1/392988993234/eknAgsglpDNs/2023/6/12/ {target_dir} --exclude "*" --include "**/160p30/*.ts" --recursive'
     subprocess.run(command, shell=True, check=True)
 
+    # ts파일들을 폴더 구조를 파괴하며 시간순으로 정렬하여 옮김
     command = f'find {target_dir} -type f -name "*.ts" -exec sh -c \'mv "$0" "{target_dir}/$(date -r "$0" +"%Y%m%d%H%M%S").ts"\' {{}} \\;'
     subprocess.run(command, shell=True, check=True)
 
@@ -27,17 +29,17 @@ def reUpload():
     ts_files = [f for f in os.listdir(folder_path) if f.endswith(".ts")]
     ts_files.sort(key=lambda x: os.path.getmtime(os.path.join(folder_path, x)))
 
-    # .ts 파일 경로 목록 생성
+    # ts 파일 경로 목록 생성
     ts_file_paths = [os.path.join(folder_path, f) for f in ts_files]
 
-    # .ts 파일들을 합쳐서 .mp4 파일로 변환
+    # ts 파일들을 합쳐서 .mp4 파일로 변환
     command = ["ffmpeg", "-i", "concat:" + "|".join(ts_file_paths), "-c:v", "copy", "-c:a", "copy", output_file]
     subprocess.run(command, check=True)
 
     print("변환 완료")
 
+    # mp4파일을 s3에 재업로드 하고 그 객체에 접근하기 위한 key값을 가져옴
     command = f'aws s3 cp {target_dir}/{now.year}.{now.month}.{now.day}.mp4 s3://ivs-ditto/ivs/v1/392988993234/eknAgsglpDNs/2023/5/12/'
-
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     deoutput = output.decode('utf-8')
@@ -61,6 +63,7 @@ def reUpload():
 
     json_data = json.dumps(data)
 
+    # 데이터베이스에 영상의 날짜와 접근하기위한 key 저장
     response = requests.post(url, data=json_data, headers={"Content-Type": "application/json"})
 
     if response.status_code == 201:
@@ -68,6 +71,7 @@ def reUpload():
     else:
         print('fail')
         
+    # videostorage의 파일들 전부 삭제
     command = ['rm', '-rf', target_dir + '/*']
     subprocess.run(command, check=True)
         
